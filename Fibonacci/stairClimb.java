fb1ee19065a380e3ed799ec9ee21bf04336b64ad
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-06-09 19:44:54 +0530

    stairClimbing solve through  tabulation

--- Fibonacci/stairClimb.java
+++ Fibonacci/stairClimb.java
@@ -3,7 +3,7 @@ package Fibonacci;
 import java.util.Arrays;
 
 public class stairClimb {
-    // memorization code
+    // memorization code - O(n) | recursion(2^n) --> n = 10^5
     public static int countWays(int n, int ways[]) {
         if (n == 0) {
             return 1;
@@ -17,11 +17,28 @@ public class stairClimb {
         return ways[n];
     }
 
+    // Tabulation code
+    public static int countWaysTab(int n) {
+        int dp[] = new int[n + 1]; // 0 to n
+        dp[0] = 1;
+
+        // tabulation loop
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1] + 0;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5; // n=3-> 3 & n=4 -> 5 => 8
         int ways[] = new int[n + 1]; // 0 0 0 0
         Arrays.fill(ways, -1); // -1 -1 -1 -1
-        System.out.println(countWays(n, ways));
+        System.out.println(countWays(n, ways)); // memorization
+        System.out.println(countWaysTab(n)); // tabulation
     }
 
 }

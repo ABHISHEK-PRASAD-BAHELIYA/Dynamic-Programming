aaf139d4752538cac84aeb8dd343399de1161d69
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-06-09 19:00:25 +0530

    Stairclimbingcode with memorization

--- Fibonacci/stairClimb.java
+++ Fibonacci/stairClimb.java
@@ -1,25 +1,27 @@
 package Fibonacci;
 
+import java.util.Arrays;
+
 public class stairClimb {
-    public static int countWays(int n) {
+    // memorization code
+    public static int countWays(int n, int ways[]) {
         if (n == 0) {
             return 1;
         } else if (n < 0) {
             return 0;
+        } else if (ways[n] != -1) { // already calculate
+            return ways[n];
         }
-        return countWays(n - 1) + countWays(n - 2);
-    }
 
-    // public static int fib(int n) {
-    // if (n == 0 || n == 1) {
-    // return n;
-    // }
-    // return fib(n - 1) + fib(n - 2);
-    // }
+        ways[n] = countWays(n - 1, ways) + countWays(n - 2, ways);
+        return ways[n];
+    }
 
     public static void main(String[] args) {
         int n = 5; // n=3-> 3 & n=4 -> 5 => 8
-        System.out.println(countWays(n));
+        int ways[] = new int[n + 1]; // 0 0 0 0
+        Arrays.fill(ways, -1); // -1 -1 -1 -1
+        System.out.println(countWays(n, ways));
     }
 
 }
